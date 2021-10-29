e5a165170f3246af9f71ae5409b79c89ba16a89a
Marcin Sola <marcinsolaonline@gmail.com>
2021-10-29 21:28:31 +0200

    Deployable smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -45,10 +45,10 @@ module.exports = {
         },
         rinkeby: {
             url: RINKEBY_RPC_URL,
-            // accounts: [PRIVATE_KEY],
-            accounts: {
-                mnemonic: MNEMONIC,
-            },
+            accounts: [PRIVATE_KEY],
+            // // // accounts: {
+            // //     mnemonic: MNEMONIC,
+            // },
             saveDeployments: true,
         },
         ganache: {
@@ -84,7 +84,7 @@ module.exports = {
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
-            1: 0 // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
         feeCollector: {
             default: 1
